5b77a4df12241bcfbcb05e7009b2ac58f99ec011
kastastin <kastastin.main@gmail.com>
2024-10-22 14:05:20 +0300

    add top section (adaptive >360px)

--- js/script.js
+++ js/script.js
@@ -3,4 +3,20 @@ const menuList = document.querySelector(".header__menu-list");
 
 menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("menu_open");
+  // if (menuList.classList.contains("menu_open") && menuList.style.zIndex == 1) {
+  //   menuList.style.zIndex = "-1";
+  //   setTimeout(() => {
+  //     menuList.classList.remove("menu_open");
+  //   }, 150);
+  //   return;
+  // }
+
+  // menuList.classList.toggle("menu_open");
+  // if (menuList.classList.contains("menu_open")) {
+  //   setTimeout(() => {
+  //     menuList.style.zIndex = "1";
+  //   }, 300);
+  // } else {
+  //   menuList.style.zIndex = "-1";
+  // }
 });
