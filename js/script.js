ed5871ff195baea10fe3c0d248b63763023acade
kastastin <kastastin.main@gmail.com>
2024-10-25 03:37:40 +0300

    add feedback section (adaptive >360px)

--- js/script.js
+++ js/script.js
@@ -3,20 +3,21 @@ const menuList = document.querySelector(".header__menu-list");
 
 menuBtn.addEventListener("click", () => {
   menuList.classList.toggle("menu_open");
-  // if (menuList.classList.contains("menu_open") && menuList.style.zIndex == 1) {
-  //   menuList.style.zIndex = "-1";
-  //   setTimeout(() => {
-  //     menuList.classList.remove("menu_open");
-  //   }, 150);
-  //   return;
-  // }
+});
 
-  // menuList.classList.toggle("menu_open");
-  // if (menuList.classList.contains("menu_open")) {
-  //   setTimeout(() => {
-  //     menuList.style.zIndex = "1";
-  //   }, 300);
-  // } else {
-  //   menuList.style.zIndex = "-1";
-  // }
+const swiperFeedback = new Swiper(".feedback__slider", {
+  loop: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+const swiperCertificates = new Swiper(".certificates__slider", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 3,
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
